c5f9cb2f3cce1331da10b00d412a492d98e0b822
niloc132 <niloc132@d51a893a-85d8-11dd-aace-ddc5e87cd236>
2008-09-19 15:10:15 +0000

    Added rudamentry UI for an article, and a  basic splash page

---
+++ MobileWiki/Classes/ArticleViewController.h
@@ -0,0 +1,18 @@
+//
+//  ArticleViewController.h
+//  MobileWiki
+//
+//  Created by Colin Alworth on 9/18/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ArticleViewController : UIViewController {
+
+}
+
+- (id) initWithTitle:(NSString *) t;
+
+@end
---
+++ MobileWiki/Classes/SampleSplashVC.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface SampleSplashVC : UIViewController {
+
+}
+
+@end
